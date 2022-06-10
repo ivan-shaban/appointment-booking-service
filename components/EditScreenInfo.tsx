4694afe2396d91f5b00aad924b465e63d33edfab
ishaban <ishaban.flash@gmail.com>
2022-06-10 17:02:28 +0400

    reformat code

--- components/EditScreenInfo.tsx
+++ components/EditScreenInfo.tsx
@@ -1,38 +1,35 @@
 import * as WebBrowser from 'expo-web-browser'
-import {
-    StyleSheet,
-    TouchableOpacity,
-} from 'react-native'
+import { StyleSheet, TouchableOpacity } from 'react-native'
 
 import Colors from '../constants/Colors'
-import {MonoText} from './StyledText'
-import {
-    Text,
-    View,
-} from './Themed'
+import { MonoText } from './StyledText'
+import { Text, View } from './Themed'
 
-export default function EditScreenInfo({path}: { path: string }) {
+export default function EditScreenInfo({ path }: { path: string }) {
     return (
         <View>
             <View style={styles.getStartedContainer}>
                 <Text
                     style={styles.getStartedText}
                     lightColor="rgba(0,0,0,0.8)"
-                    darkColor="rgba(255,255,255,0.8)">
+                    darkColor="rgba(255,255,255,0.8)"
+                >
                     Open up the code for this screen:
                 </Text>
 
                 <View
                     style={[styles.codeHighlightContainer, styles.homeScreenFilename]}
                     darkColor="rgba(255,255,255,0.05)"
-                    lightColor="rgba(0,0,0,0.05)">
+                    lightColor="rgba(0,0,0,0.05)"
+                >
                     <MonoText>{path}</MonoText>
                 </View>
 
                 <Text
                     style={styles.getStartedText}
                     lightColor="rgba(0,0,0,0.8)"
-                    darkColor="rgba(255,255,255,0.8)">
+                    darkColor="rgba(255,255,255,0.8)"
+                >
                     Change any of the text, save the file, and your app will automatically update.
                 </Text>
             </View>
