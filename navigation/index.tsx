c5dca02125b312592a2b182d7accd4ecbc73feda
ishaban <ishaban.flash@gmail.com>
2022-06-11 22:13:51 +0400

    remove depricated code

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -1,11 +1,6 @@
-import { MaterialCommunityIcons } from '@expo/vector-icons'
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs'
-import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import * as React from 'react'
-import { useState } from 'react'
-import { ColorSchemeName, Platform, View } from 'react-native'
-import { BottomNavigation, Text } from 'react-native-paper'
 
 import { ScreenHeader } from '../components/ScreenHeader'
 import { colorByTab } from '../constants/Colors'
@@ -16,8 +11,7 @@ import { MastersScreen } from '../screens/MastersScreen'
 import ModalScreen from '../screens/ModalScreen'
 import NotFoundScreen from '../screens/NotFoundScreen'
 import { ProfileScreen } from '../screens/ProfileScreen'
-import { RootStackParamList, RootTabParamList, RootTabScreenProps } from '../types'
-import LinkingConfiguration from './LinkingConfiguration'
+import { RootStackParamList, RootTabParamList } from '../types'
 
 const Stack = createNativeStackNavigator<RootStackParamList>()
 const BottomTab = createMaterialBottomTabNavigator<RootTabParamList>()
@@ -41,41 +35,6 @@ export default function Navigation() {
 }
 
 function BottomTabNavigator() {
-    // const [index, setIndex] = useState(0)
-    // const [routes] = useState([
-    //     {
-    //         key: 'masters',
-    //         title: 'Masters',
-    //         icon: 'account-group-outline',
-    //         color: '#ff0000',
-    //         badge: 2,
-    //     },
-    //     {
-    //         key: 'locations',
-    //         title: 'Locations',
-    //         icon: 'map-marker-multiple-outline',
-    //         color: '#00ff00',
-    //     },
-    //     { key: 'favourite', title: 'Favourite', icon: 'cards-heart-outline', color: '#0000ff' },
-    //     { key: 'profile', title: 'Profile', icon: 'account', color: '#0000ff' },
-    // ])
-    //
-    // const renderScene = BottomNavigation.SceneMap({
-    //     masters: MusicRoute,
-    //     locations: AlbumsRoute,
-    //     favourite: RecentsRoute,
-    //     profile: RecentsRoute,
-    // })
-    //
-    // return (
-    //     <BottomNavigation
-    //         shifting={true}
-    //         navigationState={{ index, routes }}
-    //         onIndexChange={setIndex}
-    //         renderScene={renderScene}
-    //     />
-    // )
-
     return (
         <BottomTab.Navigator
             initialRouteName={Tab.Masters}
