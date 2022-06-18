5d8b3a79e4665bd991596b07f0392851f283ab3f
ishaban <ishaban.flash@gmail.com>
2022-06-18 23:08:25 +0400

    перенес аватар пользователя в стор

--- src/components/Header/RootHeader.tsx
+++ src/components/Header/RootHeader.tsx
@@ -1,30 +1,26 @@
 import { NativeStackHeaderProps } from '@react-navigation/native-stack'
 import { useStore } from 'effector-react'
-import React, { FC, useCallback } from 'react'
+import React, { FC } from 'react'
 import { StyleSheet, TouchableOpacity } from 'react-native'
 import { Appbar, Avatar } from 'react-native-paper'
 
-import { $language, changeLanguage, languages } from '../../store/locale'
+import { $currentUser } from '../../store/user'
 
 export interface Props extends NativeStackHeaderProps {}
 
 export const RootHeader: FC<Props> = ({ navigation }) => {
-    const language = useStore($language)
-    const handleChangeLanguage = useCallback(() => {
-        const nextLangIndex = (languages.indexOf(language) + 1) % languages.length
-        changeLanguage(languages[nextLangIndex])
-    }, [language])
+    const user = useStore($currentUser)
 
     return (
         <Appbar.Header style={styles.base}>
             <Appbar.Action icon="menu" onPress={() => {}} />
             <Appbar.Action icon="magnify" style={styles.magnify} onPress={() => {}} />
             <Appbar.Action icon="bell" onPress={() => {}} />
-            <TouchableOpacity onPress={handleChangeLanguage}>
+            <TouchableOpacity onPress={() => {}}>
                 <Avatar.Image
                     size={40}
                     source={{
-                        uri: 'https://pbs.twimg.com/profile_images/952545910990495744/b59hSXUd_400x400.jpg',
+                        uri: user?.avatar,
                     }}
                 />
             </TouchableOpacity>
