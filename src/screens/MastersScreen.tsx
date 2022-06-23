81074b467577ed47d51340bfe19c5280d6fe1b09
ishaban <ishaban.flash@gmail.com>
2022-06-23 13:35:21 +0400

    release version 0.0.16

--- src/screens/MastersScreen.tsx
+++ src/screens/MastersScreen.tsx
@@ -4,12 +4,16 @@ import { StyleSheet } from 'react-native'
 import { MasterItem } from '../components/MasterItem'
 import { ScrollView } from '../components/Themed'
 import { Tab } from '../constants/Tab'
-import { $masters } from '../store/masters'
+import { $masters, $sortedMasters } from '../store/masters'
 import { RootTabScreenProps } from '../types'
 
 export function MastersScreen({ navigation }: RootTabScreenProps<Tab.Masters>) {
-    const masters = useStore($masters)
-
+    // const masters = useStore($masters)
+    const masters = useStore($sortedMasters)
+    console.log(
+        `>> masters`,
+        masters.map(({ name }) => name),
+    )
     return (
         <ScrollView style={styles.container}>
             {masters.map((master, index) => (
--- src/store/masters.ts
+++ src/store/masters.ts
@@ -2,8 +2,9 @@ import { ClientType, allGenders } from '../constants/genders'
 import { MasterLanguage, MasterType } from '../constants/masters'
 import { HairServices, Service } from '../constants/services'
 import { delay } from '../utils/time'
+import { $mastersSorting, SortOrder } from './sorting'
 import faker from '@faker-js/faker'
-import { createEffect, createStore } from 'effector'
+import { combine, createEffect, createStore } from 'effector'
 import shuffle from 'lodash.shuffle'
 
 export const allMasterTypes = [
@@ -85,4 +86,38 @@ export const requestAllMastersData = createEffect({
 export const $masters = createStore<Master[]>([]).on(
     requestAllMastersData.doneData,
     (_, data) => data,
+)
+
+export const $sortedMasters = combine(
+    $masters,
+    $mastersSorting,
+    (masters, { name, rating, feedbacks }) => {
+        // console.log(`>> 111`, { name, rating, feedbacks })
+        if (feedbacks !== SortOrder.NONE) {
+            masters = [...masters].sort((a, b) =>
+                feedbacks === SortOrder.ASC
+                    ? b.feedbacks.length - a.feedbacks.length
+                    : a.feedbacks.length - b.feedbacks.length,
+            )
+        }
+        if (rating !== SortOrder.NONE) {
+            masters = [...masters].sort((a, b) =>
+                rating === SortOrder.ASC ? b.rating - a.rating : a.rating - b.rating,
+            )
+        }
+        if (name !== SortOrder.NONE) {
+            masters = [...masters].sort((a, b) =>
+                name === SortOrder.ASC
+                    ? a.name.localeCompare(b.name)
+                    : b.name.localeCompare(a.name),
+            )
+        }
+        console.log(
+            `>> masters`,
+            masters.map(
+                ({ name, rating, feedbacks }) => `${name} (${rating}) > ${feedbacks.length}`,
+            ),
+        )
+        return masters
+    },
 )
